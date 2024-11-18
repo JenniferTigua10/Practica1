bbb9570a239c530cc762641661fbcdab00712e09
JenniferTigua10 <e1350099659@live.uleam.edu.ec>
2024-11-18 14:32:17 -0500

    Add files via upload

---
+++ Practica6Parcial2/src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { PronosticoComponent } from './pages/pronostico/pronostico.component';
+
+export const routes: Routes = [
+    {
+        //localhost:4200/pronostico
+        path: 'pronostico', component: PronosticoComponent
+    }
+];
